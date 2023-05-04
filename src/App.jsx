49ebc63ea81313c8f5bda8b72fe9edfad46bf3dd
Jacob David Diehl <jakediehl17@gmail.com>
2023-05-04 03:49:12 -0600

    hackerlandUniversity

--- src/App.jsx
+++ src/App.jsx
@@ -87,17 +87,42 @@ return(timeFormat)
 }
 
 
-console.log(timeConversion(time))
+// console.log(timeConversion(time))
+// HackerLand University has the following grading policy:
+// Every student receives a  in the inclusive range from  to .
+// Any  less than  is a failing grade.
+// Sam is a professor at the university and likes to round each student's  according to these rules:
+
+// If the difference between the  and the next multiple of  is less than , round  up to the next multiple of .
+// If the value of  is less than , no rounding occurs as the result will still be a failing grade.
+
+const grades=[73, 67, 38, 33]
+
+function gradingStudents(grades) {
+
+  const formattedGrades=[]
+  
+  grades.map(grade=>{
+    const modulusFive=grade%5-5;
+    if(grade<=37){
+      formattedGrades.push(grade)
+    }
+    if(grade>=38 && modulusFive >= -2){
+      grade+= Math.abs(modulusFive)
+      formattedGrades.push(grade)
+    }
+
+    if(grade>=38 && modulusFive <= -3){
+      formattedGrades.push(grade)
+    }
+  })
+  return(formattedGrades)
+}
 
 
 
 
-
-
-
-
-
-
+console.log(gradingStudents(grades))
 
 
 
