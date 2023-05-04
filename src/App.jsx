7f277658a3856fcc5f0c0200884cf4cf075a4fa6
Jacob David Diehl <jakediehl17@gmail.com>
2023-05-04 03:17:05 -0600

    time Conversion

--- src/App.jsx
+++ src/App.jsx
@@ -33,10 +33,70 @@ for(let i =1; i<candles.length; i++){
 
  
 }
-console.log(maxNumber)
+return(maxNumber)
 }
 
-birthdayCakeCandles(candles)
+// console.log(birthdayCakeCandles(candles))
+
+
+
+
+
+
+
+
+const time = "12:45:54PM";
+
+
+
+
+function timeConversion(s) {
+  const length = s.length
+  const AMOrPM=(s.charAt(length-2))
+  let morning = s.slice(0,-2)
+  if(morning.slice(0,2)==="12"){
+    const sliceOfMorning=morning.slice(2)
+    const sAM= "00".concat(sliceOfMorning)
+    morning = sAM
+  }
+  let timeFormat =""
+let hours = parseInt(s.charAt(0).concat(s.charAt(1)))
+if(AMOrPM==="P"){
+  hours+=12
+  
+  if(s.slice(0,2)==="12"){
+    hours-=12
+  }
+  const stringHours= String(hours)
+  const sliceOfS= s.slice(2,-2)
+  const sPM= stringHours.concat(sliceOfS)
+  timeFormat=sPM
+
+}
+
+if(AMOrPM==="A"){
+timeFormat=morning  
+}
+
+
+
+return(timeFormat)
+
+
+
+}
+
+
+console.log(timeConversion(time))
+
+
+
+
+
+
+
+
+
 
 
 
