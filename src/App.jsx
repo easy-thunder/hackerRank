30d7da21ed4d8268fa02cdfad0761d1f8903442a
Jacob David Diehl <jakediehl17@gmail.com>
2023-05-04 12:32:35 -0600

    number line jumps

--- src/App.jsx
+++ src/App.jsx
@@ -127,38 +127,110 @@ export default App
 
 
 
-function countApplesAndOranges(s, t, a, b, apples, oranges) {
-  let numberOfApplesOnHouse=0;
-  let numberOfOrangesOnHouse=0;
+// function countApplesAndOranges(s, t, a, b, apples, oranges) {
+//   let numberOfApplesOnHouse=0;
+//   let numberOfOrangesOnHouse=0;
 
-  // Write your code here
-  apples.map(apple=>{
-    const x=apple+a;
-    if(s<=x && x<=t){
-      numberOfApplesOnHouse+=1;
-    }
-  })
-  oranges.map(orange=>{
-    const x=orange+b;
-    if(s<=x && x<=t){
-      numberOfOrangesOnHouse+=1;
-    }
-  })
-  console.log(numberOfApplesOnHouse )
-  console.log(numberOfOrangesOnHouse )
+//   // Write your code here
+//   apples.map(apple=>{
+//     const x=apple+a;
+//     if(s<=x && x<=t){
+//       numberOfApplesOnHouse+=1;
+//     }
+//   })
+//   oranges.map(orange=>{
+//     const x=orange+b;
+//     if(s<=x && x<=t){
+//       numberOfOrangesOnHouse+=1;
+//     }
+//   })
+//   console.log(numberOfApplesOnHouse )
+//   console.log(numberOfOrangesOnHouse )
 
-}
+// }
 // s=house start t=house end a= apple start b=orange start
 
-const s= 7
-const t = 11
-const a = 5
-const b = 15 
-const apples=[-2,2,1]
-const oranges = [5,-6]
+// const s= 7
+// const t = 11
+// const a = 5
+// const b = 15 
+// const apples=[-2,2,1]
+// const oranges = [5,-6]
 
 
-countApplesAndOranges(s,t,a,b,apples,oranges)
+// countApplesAndOranges(s,t,a,b,apples,oranges)
+
+
+
+//number line jumps
+
+// function kangaroo(x1, v1, x2, v2) {
+//   // Write your code here
+// const array=[{initial:x1, velocity:v1},{initial:x2, velocity:v2}]
+// const sortedArray=array.sort((a,b)=>a.initial-b.initial)
+
+// //if the velocity of the smaller initial number is smaller than the velocity of the larger number the first velocity will never catch up to the larger number if they did not start at the same place. Thus we return NO.
+// if(sortedArray[0].velocity<=sortedArray[1].velocity && sortedArray[0].initial!== sortedArray[1].initial){
+//   return "NO"
+// }
+
+// //this is to increment the numbers. I'm messing with the initial values as it doesn't matter after I start, I just need to know if the two numbers overlap with their velocity.
+// if(sortedArray[0].initial<sortedArray[1].initial){
+//   recursion()
+// }
+// function recursion(){
+//   if(sortedArray[0].initial> sortedArray[1].initial || sortedArray[0].initial===sortedArray[1].initial){return}
+//     sortedArray[0].initial+=sortedArray[0].velocity
+//     sortedArray[1].initial+=sortedArray[1].velocity
+//     recursion()
+//   }
+// //next two if statements are what I return 
+// if(sortedArray[0].initial>sortedArray[1].initial){
+//   return "NO"
+// }
+// if(sortedArray[0].initial===sortedArray[1].initial){
+//     return "YES"    
+// }
+
+// }
+
+// //x=start v=each step too the right
+
+// const x1=2;
+// const v1=1;
+// const x2=1;
+// const v2=2;
+
+// console.log(kangaroo(x1, v1, x2, v2))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
